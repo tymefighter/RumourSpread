f9aa1d02f479f19c19de8efb630a3c753cee45e6
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-05-14 00:46:47 +0530

    Add modified version of Barabasi-Albert algorithm

--- Code/scale_free.py
+++ Code/scale_free.py
@@ -1,16 +1,31 @@
-import random
+import numpy as np
 
 from graph import Graph
 
-def generate_spanning_tree(n):
+def initialize_graph(n, init_num_nodes):
     
     spanning_tree = Graph(n)
 
-    for i in range(1, n):
-        j = random.randint(0, i - 1)
+    for i in range(1, init_num_nodes):
+        j = np.random.randint(0, i)
         spanning_tree.add_edge(i, j)
 
     return spanning_tree
 
 def generate_scale_free(n, init_num_nodes, num_edges_per_step):
-    pass
+    
+    graph = initialize_graph(n, init_num_nodes)
+
+    deg_list = np.zeros(n, dtype=np.int32)
+    deg_list[:init_num_nodes] = 1
+
+    for i in range(init_num_nodes, n):
+        deg_prob = deg_list[:i]
+        deg_prob /= deg_prob.sum()
+
+        verts = np.random.choice(i, num_edges_per_step, False, deg_prob)
+
+        for j in verts:
+            graph.add_edge(i, j)
+
+    return graph
