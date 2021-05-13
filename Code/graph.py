46f857a0e221b63482d40100c188cb94f3db7ef2
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-05-14 00:15:29 +0530

    Add degree computation

--- Code/graph.py
+++ Code/graph.py
@@ -2,8 +2,24 @@ class Graph:
 
     def __init__(self, n):
 
+        self.n = n
         self.adj_list = [[] for i in range(n)]
 
     def add_edge(self, u, v):
 
         self.adj_list[u].append(v)
+
+    def compute_degrees(self):
+
+        return [len(self.adj_list[i]) for i in range(self.n)]
+
+
+def compute_distribution(deg_list):
+
+    n = len(deg_list)
+    deg_dist = [0] * n
+
+    for deg in deg_list:
+        deg_dist[deg] += 1
+
+    return deg_dist
