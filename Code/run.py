f7942e39038890423434f9cb47f5a5595bcf4ead
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-05-14 21:53:30 +0530

    Add sample test for opinion fragmentation

--- Code/run.py
+++ Code/run.py
@@ -1,6 +1,6 @@
 from rumour_spread_model import RumourSpreadModel
-from callback import RangeOfInformationSpread
-from plot import plot_range_of_info_spread
+from callback import RangeOfInformationSpread, OpinionFragmentation
+from plot import plot_range_of_info_spread, plot_opinion_fragmentation
 
 def main():
 
@@ -8,15 +8,19 @@ def main():
     num_bits = 5
     timesteps = 100
     
-    rumour_spread = RumourSpreadModel(num_nodes, num_bits, 100, 1, 1)
+    rumour_spread = RumourSpreadModel(num_nodes, num_bits, 100, 1, -3)
     result_list = rumour_spread.simulate(
         {0: 0}, timesteps, 
         [RangeOfInformationSpread(
+            num_nodes, num_bits, timesteps
+        ),
+        OpinionFragmentation(
             num_nodes, num_bits, timesteps
         )]
     )
 
     plot_range_of_info_spread(result_list[0])
+    plot_opinion_fragmentation(result_list[1])
 
 if __name__ == '__main__':
     main()
