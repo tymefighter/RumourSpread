4ff4166fc5b4ce2ba6e0dbdf77abd58789953182
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-05-14 22:16:09 +0530

    Add test case for entropy plot

--- Code/run.py
+++ Code/run.py
@@ -1,6 +1,15 @@
 from rumour_spread_model import RumourSpreadModel
-from callback import RangeOfInformationSpread, OpinionFragmentation
-from plot import plot_range_of_info_spread, plot_opinion_fragmentation
+from callback import (
+    RangeOfInformationSpread,
+    OpinionFragmentation,
+    AverageInformationEntropy
+)
+
+from plot import (
+    plot_range_of_info_spread,
+    plot_opinion_fragmentation,
+    plot_avg_info_entropy
+)
 
 def main():
 
@@ -16,11 +25,15 @@ def main():
         ),
         OpinionFragmentation(
             num_nodes, num_bits, timesteps
+        ),
+        AverageInformationEntropy(
+            num_nodes, timesteps
         )]
     )
 
     plot_range_of_info_spread(result_list[0])
     plot_opinion_fragmentation(result_list[1])
+    plot_avg_info_entropy(result_list[2])
 
 if __name__ == '__main__':
     main()
