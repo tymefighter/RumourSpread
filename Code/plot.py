6f6f8cbbdb267ac106ea4e8357211920fc136a0d
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-05-14 22:44:23 +0530

    Minor fixes

--- Code/plot.py
+++ Code/plot.py
@@ -9,8 +9,8 @@ def plot_degree_distribution(degree_distribution, max_degree):
     plt.plot(degree_distribution[:max_degree + 1])
     plt.show()
 
-def plot_range_of_info_spread(bit_counts):
-    
+def plot_bit_counts(bit_counts, title):
+
     num_bit_strings = len(bit_counts[0])
     num_bits = int(math.log2(num_bit_strings))
     bit_string_list = [None] * num_bit_strings
@@ -23,14 +23,18 @@ def plot_range_of_info_spread(bit_counts):
     )
 
     sns.heatmap(bit_count_df)
+    plt.title(title)
     plt.show()
 
+def plot_range_of_info_spread(
+    range_of_info_spread, title='Range of Info Spread'
+):  
+    plot_bit_counts(range_of_info_spread, title)
 
-def plot_opinion_fragmentation(opinion_freq):
+def plot_opinion_fragmentation(opinion_freq, title='Opinion Frequency'):
 
-    plot_range_of_info_spread(opinion_freq)
+    plot_bit_counts(opinion_freq, title)
 
-    
 def plot_avg_info_entropy(avg_entropy):
 
     plt.plot(avg_entropy)
