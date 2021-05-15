ebd5ec4e1e764547a662ab271f59266674323961
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-05-15 23:48:18 +0530

    Add simulation

--- Code/plot.py
+++ Code/plot.py
@@ -9,14 +9,21 @@ def plot_degree_distribution(degree_distribution, max_degree):
     plt.plot(degree_distribution[:max_degree + 1])
     plt.show()
 
-def plot_bit_counts(bit_counts_list, title_list):
+def plot_bit_counts(
+    bit_counts_list, title_list, 
+    suptitle, num_plots_per_row=2
+):
+    assert num_plots_per_row > 1
 
-    num_rows = (len(bit_counts_list) + 1) // 2
-    fig, axes = plt.subplots(nrows=num_rows, ncols=2, figsize=(12, 8))
+    num_rows = (len(bit_counts_list) + 1) // num_plots_per_row
+    fig, axes = plt.subplots(
+        nrows=num_rows, ncols=num_plots_per_row, figsize=(16, 10)
+    )
 
     for idx, bit_counts in enumerate(bit_counts_list):
 
-        ax = axes[idx % 2] if num_rows == 1 else axes[idx // 2][idx % 2]
+        ax = axes[idx % num_plots_per_row] if num_rows == 1 \
+            else axes[idx // num_plots_per_row][idx % num_plots_per_row]
 
         num_bit_strings = len(bit_counts[0])
         num_bits = int(math.log2(num_bit_strings))
@@ -32,29 +39,32 @@ def plot_bit_counts(bit_counts_list, title_list):
         sns.heatmap(bit_count_df, ax=ax, cmap='viridis')
         ax.set_title(title_list[idx])
 
-    if len(bit_counts_list) % 2 == 1:
+    for j in range(idx + 1, len(bit_counts_list)):
         fig.delaxes(
-            axes[1] if num_rows == 1 else axes[len(bit_counts_list // 2)][1]
+            axes[idx % num_plots_per_row] if num_rows == 1
+            else axes[idx // num_plots_per_row][idx % num_plots_per_row]
         )
 
+    fig.suptitle(suptitle)
     plt.show()
 
 def plot_range_of_info_spread(
     range_of_info_spread_list, 
-    title_list
+    title_list, suptitle,
+    num_plots_per_row=2
 ):  
-    plot_bit_counts(range_of_info_spread_list, title_list)
+    plot_bit_counts(range_of_info_spread_list, title_list, 
+        suptitle, num_plots_per_row)
 
 def plot_opinion_fragmentation(
     opinion_freq_list, 
-    title_list
+    title_list, suptitle,
+    num_plots_per_row=2
 ):
-    plot_bit_counts(opinion_freq_list, title_list)
+    plot_bit_counts(opinion_freq_list, title_list, 
+        suptitle, num_plots_per_row)
 
-def plot_avg_info_entropy(
-    avg_entropy_list, label_list, 
-    title
-):
+def plot_avg_info_entropy(avg_entropy_list, label_list, title):
     for i in range(len(avg_entropy_list)):
         plt.plot(avg_entropy_list[i], label=label_list[i])
 
